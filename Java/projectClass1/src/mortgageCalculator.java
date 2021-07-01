693edf076ef04c82bd82ed0688e28a5e5f040d33
DiegoxK <diego.stv@outlook.es>
2021-07-01 11:35:49 -0500

    mortgage

--- Java/projectClass1/src/mortgageCalculator.java
+++ Java/projectClass1/src/mortgageCalculator.java
@@ -8,16 +8,42 @@ public class mortgageCalculator {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        
+        int principal;
+        while (true) {
 
-        scanner = new Scanner(System.in);
-        System.out.print("Annual Interest Rate (%): ");
-        float interestRate = scanner.nextFloat();
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
 
-        scanner = new Scanner(System.in);
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
+
+        float interestRate;
+        while(true){
+
+            System.out.print("Annual Interest Rate (%): ");
+            interestRate = scanner.nextFloat();
+
+            if (interestRate > 0 && interestRate <= 30)
+                    break;
+
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+
+        byte period;
+        while(true){
+
+            System.out.print("Period (Years): ");
+            period = scanner.nextByte();
+
+            if (period > 0 && period <= 30)
+                    break;
+
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         float monthlyInterest = interestRate/PERCENT/MONTHS_OF_THE_YEAR;
         double numberOfPayments = period * MONTHS_OF_THE_YEAR;
