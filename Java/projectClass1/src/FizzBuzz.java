8d86c48c57d2f9617a4e36d951d5f802aba828b7
DiegoxK <diego.stv@outlook.es>
2021-06-30 19:07:42 -0500

    fizzBuzz

---
+++ Java/projectClass1/src/FizzBuzz.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("number: ");
+        int number = scan.nextInt();
+
+        if (number%5 == 0 && number%3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number%3 == 0)
+            System.out.println("Buzz");
+        else if (number%5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
+
+    }
+}
